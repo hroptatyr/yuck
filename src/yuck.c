0873a737ae6c9708b983dd39fa4fea244b30315e
Sebastian Freundt <freundt@ga-group.nl>
2013-11-29 16:32:57 +0000

    minor, kick off bootstrapping of yuck binary
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/yuck.c
+++ src/yuck.c
@@ -482,6 +482,7 @@ divert[]dnl");
 }
 
 
+#if defined BOOTSTRAP
 int
 main(int argc, char *argv[])
 {
@@ -500,5 +501,28 @@ main(int argc, char *argv[])
 
 	return -rc;
 }
+#endif	/* BOOTSTRAP */
+
+
+#if !defined BOOTSTRAP
+#include "yuck.yh"
+#include "yuck.yc"
+
+int
+main(int argc, char *argv[])
+{
+	struct yuck_s argi[1U];
+	int rc = 0;
+
+	if (yuck_parse(argi, argc, argv) < 0) {
+		rc = 1;
+		goto out;
+	}
+
+out:
+	yuck_free(argi);
+	return rc;
+}
+#endif	/* !BOOTSTRAP */
 
 /* yuck.c ends here */
