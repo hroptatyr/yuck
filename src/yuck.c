441e5d76f21bc4020813303104e39e0a378c624d
Sebastian Freundt <freundt@ga-group.nl>
2013-12-04 15:42:49 +0000

    minor, support optional arguments syntax is --option[=FOO]
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/yuck.c
+++ src/yuck.c
@@ -83,6 +83,8 @@ struct opt_s {
 	char *lopt;
 	char *larg;
 	char *desc;
+	unsigned int oarg:1U;
+	unsigned int marg:1U;
 };
 
 
@@ -292,6 +294,21 @@ usagep(const char *line, size_t llen)
 	return 1;
 }
 
+static bool
+isdashdash(const char c)
+{
+	switch (c) {
+	default:
+		return true;
+	case '=':
+	case '[':
+	case ']':
+	case '.':
+	case '\0' ... ' ':
+		return false;
+	}
+}
+
 static int
 optionp(const char *line, size_t llen)
 {
@@ -366,14 +383,28 @@ yield:
 	if (*sp++ == '-') {
 		const char *op;
 
-		for (op = sp; sp < ep && !isspace(*sp) && *sp != '='; sp++);
+		for (op = sp; sp < ep && isdashdash(*sp); sp++);
 		cur_opt.lopt = bbuf_cpy(lopt, op, sp - op);
 
-		if (*sp++ == '=') {
+		switch (*sp++) {
+		case '[':
+			if (*sp++ != '=') {
+				/* just bullshit then innit? */
+				break;
+			}
+			/* otherwise optarg, fall through */
+			cur_opt.oarg = 1U;
+		case '=':;
 			/* has got an arg */
 			const char *ap;
-			for (ap = sp; sp < ep && !isspace(*sp); sp++);
+			for (ap = sp; sp < ep && isdashdash(*sp); sp++);
 			cur_opt.larg = bbuf_cpy(larg, ap, sp - ap);
+			if (cur_opt.oarg && *sp++ != ']') {
+				/* maybe not an optarg? */
+				;
+			}
+		default:
+			break;
 		}
 	}
 	/* require at least one more space? */
@@ -483,8 +514,10 @@ yield_opt(const struct opt_s *arg)
 		fprintf(outf, "yuck_add_option([%s], [%s], [flag], [%s]);\n",
 			sopt, opt, cmd);
 	} else {
-		fprintf(outf, "yuck_add_option([%s], [%s], [arg, %s], [%s]);\n",
-			sopt, opt, arg->larg, cmd);
+		const char *atypes[] = {"arg", "oarg", "marg", "moarg"};
+		const char *atype = atypes[arg->oarg | arg->marg << 1U];
+		fprintf(outf, "yuck_add_option([%s], [%s], [%s, %s], [%s]);\n",
+			sopt, opt, atype, arg->larg, cmd);
 	}
 	if (arg->desc != NULL) {
 		/* kick last newline */
