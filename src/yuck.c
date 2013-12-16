8da949ab612e824ea20aedd389f7226ad8637c47
Sebastian Freundt <freundt@ga-group.nl>
2013-12-16 08:29:06 +0000

    Revert "minor, also call m4 internally with -B option"
    This reverts commit c6c4455271b3476497312ec65f126c4cdb2c4649.

--- src/yuck.c
+++ src/yuck.c
@@ -941,8 +941,8 @@ find_dsl(void)
 static __attribute__((noinline)) int
 run_m4(const char *outfn, ...)
 {
-	static char *m4_cmdline[8U] = {
-		"m4", "-B", "16386", dslfn,
+	static char *m4_cmdline[6U] = {
+		"m4", dslfn,
 	};
 	va_list vap;
 	pid_t m4p;
@@ -978,7 +978,7 @@ run_m4(const char *outfn, ...)
 	}
 	/* child code here */
 	va_start(vap, outfn);
-	for (size_t i = 4U;
+	for (size_t i = 2U;
 	     i < countof(m4_cmdline) &&
 		     (m4_cmdline[i] = va_arg(vap, char*)) != NULL; i++);
 	va_end(vap);
