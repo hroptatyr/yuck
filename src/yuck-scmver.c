eb69a962f716c35ce81d2a216d1b0de002f6577d
Sebastian Freundt <freundt@ga-group.nl>
2014-01-17 10:14:30 +0000

    minor, allow yuck-scmver tool (bootstrapped) to read a reference file (second arg)
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/yuck-scmver.c
+++ src/yuck-scmver.c
@@ -770,17 +770,22 @@ static const char *yscm_strs[] = {
 int
 main(int argc, char *argv[])
 {
+/* usage would be yuck-scmver SCMDIR [REFERENCE] */
 	static struct yuck_version_s v[1U];
-	const char *infn = NULL;
 	int rc = 0;
 
-	if (argc > 1) {
-		infn = argv[1U];
-	}
-	if ((rc = yuck_version(v, infn)) < 0) {
-		/* pity */
-		;
-	} else {
+	switch ((rc = yuck_version(v, argv[1U]))) {
+	default:
+		/* one more chance there is */
+		if (argc <= 2) {
+			/* no reference file given */
+			break;
+		} else if ((rc = yuck_version_read(v, argv[2U])) < 0) {
+			/* ok, can't help it then */
+			break;
+		}
+		/* yay, fallthrough to success */
+	case 0:
 		fputs("define(YUCK_SCMVER_VERSION, ", stdout);
 		fputs(v->vtag, stdout);
 		if (v->scm > YUCK_SCM_TARBALL && v->dist) {
@@ -792,6 +797,7 @@ main(int argc, char *argv[])
 			fputs(".dirty", stdout);
 		}
 		fputs(")\n", stdout);
+		break;
 	}
 	return -rc;
 }
